2155b466584fe72d6bded18d5c8f09809b142a11
frimpongopoku <pongofrimi@gmail.com>
2021-03-07 12:58:59 +0400

    Fixed modal toggling and currently adding dummy mentors as json

---
+++ js/db/mentors.js
@@ -0,0 +1,110 @@
+const lorem = `Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi
+doloremque fuga obcaecati facere, enim quisquam, temporibus optio
+ducimus magni aliquam nulla. Eligendi officia eaque nesciunt facilis
+quia consectetur totam non.
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi
+doloremque fuga obcaecati facere, enim quisquam, temporibus optio
+ducimus magni aliquam nulla. Eligendi officia eaque nesciunt facilis
+quia consectetur totam non.`;
+
+const mentors = [
+  {
+    section: "gh-section",
+    country: "Ghana",
+    mentors: [
+      {
+        name: "Samuel Opoku Agyemang",
+        job: "Software Engineer",
+        company: "Slack",
+        email: "sam@gmail.com",
+        phone: "+233 456 789 43",
+        website: "sam.website.com",
+        bio: lorem,
+      },
+      {
+        name: "Charles Opoku Agyemang",
+        job: "Software Developer",
+        company: "Slack",
+        bio: lorem,
+        email: "sam@gmail.com",
+        phone: "+233 456 789 43",
+        website: "sam.website.com",
+      },
+      {
+        name: "Tahiru Abdullai",
+        job: "Mobile App Developer",
+        company: "Slack",
+        bio: lorem,
+        email: "sam@gmail.com",
+        phone: "+233 456 789 43",
+        website: "sam.website.com",
+      },
+      {
+        name: "Faddal Ibrahim",
+        job: "Full Stack Developer",
+        company: "Slack",
+        bio: lorem,
+        email: "sam@gmail.com",
+        phone: "+233 456 789 43",
+        website: "sam.website.com",
+      },
+      {
+        name: "Assan Ewudzi Nathaniel",
+        job: "Full Stack Developer",
+        company: "Slack",
+        bio: lorem,
+        email: "sam@gmail.com",
+        phone: "+233 456 789 43",
+        website: "sam.website.com",
+      },
+    ],
+  },
+  {
+    section: "mu-section",
+    country: "Mauritius",
+    mentors: [
+      {
+        name: "Mani Riaz",
+        job: "Software Engineer",
+        company: "Slack",
+        bio: lorem,
+        email: "sam@gmail.com",
+        phone: "+233 456 789 43",
+        website: "sam.website.com",
+      },
+      {
+        name: "Sania Sooraj",
+        job: "Software Developer",
+        company: "Slack",
+        bio: lorem,
+      },
+      {
+        name: "Sharon Ramgoolam",
+        job: "Mobile App Developer",
+        company: "Slack",
+        bio: lorem,
+        email: "sam@gmail.com",
+        phone: "+233 456 789 43",
+        website: "sam.website.com",
+      },
+      {
+        name: "Raychel Soulayman",
+        job: "Full Stack Developer",
+        company: "Slack",
+        bio: lorem,
+        email: "sam@gmail.com",
+        phone: "+233 456 789 43",
+        website: "sam.website.com",
+      },
+      {
+        name: "Fabrice Bilaal",
+        job: "Full Stack Developer",
+        company: "Slack",
+        bio: lorem,
+        email: "sam@gmail.com",
+        phone: "+233 456 789 43",
+        website: "sam.website.com",
+      },
+    ],
+  },
+];
--- js/index.js
+++ js/index.js
@@ -1,10 +1,15 @@
+
+
 const grab = (id) => document.getElementById(id);
 const arrow = grab("arrow-direction");
 const applyMenuBtn = grab("apply-menu-btn");
 const appForm = grab("app-form-container");
 const overlay = grab("overlay");
-
+const modal = grab("modal-holder");
+const test = grab("test");
 var formToggled = false;
+var modalToggled = false;
+
 const openAppForm = () => {
   if (!formToggled) {
     // If its not toggled, open up the form
@@ -23,13 +28,31 @@ const openAppForm = () => {
   formToggled = !formToggled;
 };
 
-const generateMentorsForSection = (many,id) => {
+const generateMentorsForSection = (many, id) => {
   const section = grab(id);
   for (let i = 0; i < many; i++) {
     section.appendChild(generateMentorBox());
   }
 };
 
-generateMentorsForSection(4,"mu-section");
-generateMentorsForSection(4,"gh-section")
-generateMentorsForSection(4,"uk-section");
+generateMentorsForSection(4, "mu-section");
+generateMentorsForSection(4, "gh-section");
+generateMentorsForSection(4, "uk-section");
+
+const toggleModal = () => {
+  if (!modalToggled) {
+    modal.classList.add("show-modal");
+    modal.classList.remove("modal-off");
+    modalToggled = true;
+    overlay.style.display = "block";
+    // overlay.style.style.zIndex = 21;
+  } else {
+    modal.classList.remove("show-modal");
+    modal.classList.add("modal-off");
+    modalToggled = false;
+    overlay.style.display = "none";
+    // overlay.style.style.zIndex = 2;
+  }
+};
+
+console.log(mentors);
